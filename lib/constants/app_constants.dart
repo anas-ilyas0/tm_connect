930bbdcd302618a4f0369cd256f3a2f55384e74c
anas-ilyas0 <manasfaizan0786@gmail.com>
2025-10-30 16:24:18 +0500

    url changed

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -3,7 +3,7 @@ class AppConstants {
   static const screenH = 844.0;
   static const String appName = 'TMConnect';
   static const String tmConnectUrl =
-      'http://staging.waterstreet.net/tmconnect_v3/mobile';
+      'https://sandbox.waterstreet.net/tmconnect_v3/mobile/secure.cfm';
   static const String privacyPolicy =
       'https://staging.waterstreet.net/tmconnect/privacy_statement.html';
   static const String poppins = 'Poppins';
